fa15e7e8c973f0f60ace791eee88459b41de5e2a
Jose Morales <josea.91@hotmail.com>
2025-10-13 08:48:39 -0500

    Create Button component using props.children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Button from './components/Button.jsx';
 
 function App() {
   return (
     <div className="App">
       <div className='container-calculator'>
-        <div className='row'></div>
+        <div className='row'>
+        <Button>1</Button>
+
+        </div>
         <div className='row'></div>
         <div className='row'></div>
         <div className='row'></div>
